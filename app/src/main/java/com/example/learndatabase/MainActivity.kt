6344c03d3d8d17154edade800f7ea118fdd7a785
BagasYudha <bayud.kematian@gmail.com>
2024-11-18 12:15:24 +0700

    Membuat navigation fragment

--- app/src/main/java/com/example/learndatabase/MainActivity.kt
+++ app/src/main/java/com/example/learndatabase/MainActivity.kt
@@ -3,12 +3,27 @@ package com.example.learndatabase
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.learndatabase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        // Menghubungkan NavController dengan NavHostFragment
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        // Setup BottomNavigationView dengan NavController
+        binding.bottomBar.setupWithNavController(navController)
 
     }
 }
