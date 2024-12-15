70899b7cd7380417fcff04297c542be64813977a
BagasYudha <bayud.kematian@gmail.com>
2024-12-16 00:56:21 +0700

    Menambahkan fitur login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
 
     // Testing
     testImplementation(libs.junit)
